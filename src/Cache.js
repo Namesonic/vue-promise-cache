14930ae41daf3c360413676bdcc1d4b35ba49c68
Jason <jason@namesonic.com>
2018-01-27 23:10:22 -0800

    splits Class assets into separate files and renames

--- src/Cache.js
+++ src/Cache.js
@@ -1,44 +1,5 @@
-class CachedItem {
-  constructor (id) {
-    this._ready = false
-    this._error = false
-    this._errormsg = ''
-    this._id = id
-    this.data = {}
-  }
+import CachedItem from './CachedItem'
 
-  update (data) {
-    this.data = data
-    // Object.assign(this, {...data})
-  }
-
-  set ready (payload) {
-    this._ready = payload ? true : false
-  }
-
-  set error (err) {
-    if (err) {
-      this._error = true
-      this._errormsg = err
-    }
-  }
-
-  get id () {
-    return this._id
-  }
-
-  get isReady () {
-    return this._ready
-  }
-
-  get error () {
-    return this._errormsg
-  }
-
-  get hasError () {
-    return this._error
-  }
-}
 class Cache {
   constructor (callback) {
     this._items = []
